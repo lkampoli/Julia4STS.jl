f11875854ce80582b1ecb79cf131d29ad4bee60a
Documenter.jl <documenter@juliadocs.github.io>
2022-04-14 04:53:22 +0000

    build based on 086fa6c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Julia4STS","category":"page"},{"location":"#Julia4STS","page":"Home","title":"Julia4STS","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Julia4STS.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Julia4STS]","category":"page"}]
+}
